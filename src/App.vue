1980aad7f8ba17efaf24c759251e25ce5d4bdfaf
juan <juan.vargasva@gmail.com>
2017-12-29 21:13:00 -0500

    dsd

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,34 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>HI WORLD</h2>
-    <p>{{msg1}}</p>
-    <ul>
-      <li><router-link to="/home">Home</router-link></li>
-      <li><router-link to="/contacto">Contacto</router-link></li>
-      <li><router-link to="/">/</router-link></li>
-    </ul>
-    <router-view></router-view>
-    <h2>Ecosystem</h2>
-   <!--  <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+
+      <li class="nav-item"><router-link to="/home">Home</router-link></li>
+      <li class="nav-item"><router-link to="/contacto">Contacto</router-link></li>
+      <li class="nav-item"><router-link to="/">/</router-link></li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+   
+   <div class="container">
+    <br>
+    <br>
+    <br>
+    <br>
+      <router-view></router-view>
+   </div>
+   
   </div>
 </template>
 
@@ -33,30 +45,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
